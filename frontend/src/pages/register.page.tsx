7ce247f55731dfdfc46b2854a5b1a7841c4bb9d4
Edem <ziddah.edem@gmail.com>
2022-08-02 17:03:58 +0000

    updated

--- frontend/src/pages/register.page.tsx
+++ frontend/src/pages/register.page.tsx
@@ -31,15 +31,15 @@ const LinkItem = styled(Link)`
 `;
 
 const registerSchema = object({
-  name: string().nonempty('Full name is required').max(100),
+  name: string().min(1, 'Full name is required').max(100),
   email: string()
-    .nonempty('Email address is required')
+    .min(1, 'Email address is required')
     .email('Email Address is invalid'),
   password: string()
-    .nonempty('Password is required')
+    .min(1, 'Password is required')
     .min(8, 'Password must be more than 8 characters')
     .max(32, 'Password must be less than 32 characters'),
-  passwordConfirm: string().nonempty('Please confirm your password'),
+  passwordConfirm: string().min(1, 'Please confirm your password'),
 }).refine((data) => data.password === data.passwordConfirm, {
   path: ['passwordConfirm'],
   message: 'Passwords do not match',
