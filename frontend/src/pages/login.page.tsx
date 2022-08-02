4686bcc17a899ccadba9604fa9032071227735b9
Edem <ziddah.edem@gmail.com>
2022-08-02 17:06:08 +0000

    updated

--- frontend/src/pages/login.page.tsx
+++ frontend/src/pages/login.page.tsx
@@ -37,10 +37,10 @@ const LinkItem = styled(Link)`
 
 const loginSchema = object({
   email: string()
-    .nonempty('Email address is required')
+    .min(1, 'Email address is required')
     .email('Email Address is invalid'),
   password: string()
-    .nonempty('Password is required')
+    .min(1, 'Password is required')
     .min(8, 'Password must be more than 8 characters')
     .max(32, 'Password must be less than 32 characters'),
 });
